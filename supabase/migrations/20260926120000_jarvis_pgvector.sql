2b613062c9d4e11d608c3bb824fd4757aced0db7
Vivek Kumar <vivekkumarfbg000@gmail.com>
2026-09-26 12:43:19 +0530

    Trigger CI: Final Password Test

--- supabase/migrations/20260926120000_jarvis_pgvector.sql
+++ supabase/migrations/20260926120000_jarvis_pgvector.sql
@@ -26,4 +26,4 @@ ALTER TABLE public.jarvis_code_embeddings ENABLE ROW LEVEL SECURITY;
 CREATE POLICY "Allow Jarvis Daemon Full Access" 
 ON public.jarvis_code_embeddings 
 FOR ALL USING (true);
--- Trigger CI: Env var password
+-- Trigger CI: Final Password Test
